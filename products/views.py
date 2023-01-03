99ba1c55b19d045a41132a7f20b3d94ccc705785
Arthur Khasiev <hasiev_artur@mail.ru>
2023-01-03 22:28:40 +0700

    static files

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html', {'title': 'Главная страница'})
+    return render(request, 'products/index.html', {'title': 'Главная страница'}) # тут и ниже title чисто для себя
 
 def products(request):
     return render(request, 'products/products.html', {'title': 'Продукты'})
