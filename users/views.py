76d168742ca8e028beb4052093942be60d412b4f
Arthur Khasiev <hasiev_artur@mail.ru>
2023-01-11 05:13:30 +0700

    some modification with top panel (logout, adm-panel show for admin & stuff, etc)

--- users/views.py
+++ users/views.py
@@ -47,3 +47,6 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('home'))
