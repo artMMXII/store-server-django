cabb90e65995e73513fd25aec2b6fc4494b7372c
Arthur Khasiev <hasiev_artur@mail.ru>
2023-01-16 02:59:29 +0700

    make pagination

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='home'),
     path('category/<int:category_id>/', products, name='category'),
+    path('page/<int:page_number>/', products, name='paginator'),
     path('bascets/add<int:product_id>/', basket_add, name='basket_add'),
     path('bascets/remove<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
