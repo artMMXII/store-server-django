074185ad2217c146b0031a41eb06e0f4ce6a18df
Arthur Khasiev <hasiev_artur@mail.ru>
2023-01-16 07:09:15 +0700

    modify admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,22 @@ from products.models import *
 
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+# admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
+
+
