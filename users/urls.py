7062a3a200c484e4ea6c632832b5bdac20d0408f
Arthur Khasiev <hasiev_artur@mail.ru>
2023-01-17 20:57:31 +0700

    email verification

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login', UserLoginView.as_view(), name='login'),
     path('registration', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
-    path('logkut', LogoutView.as_view(), name='logout'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('verify/<str:email>/<uuid:code>', EmailVerificationView.as_view(), name='email_verification'),
 ]
